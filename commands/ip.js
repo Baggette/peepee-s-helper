b8f1c45d1cf4a867855c2b5107b57c24e1696cc9
gabe1cragger <84940492+gabe1cragger@users.noreply.github.com>
2022-07-14 00:58:04 -0400

    Update ip.js

--- commands/ip.js
+++ commands/ip.js
@@ -7,9 +7,9 @@ module.exports ={
         .setColor("#FF0000")
         .setAuthor({name:"Peepee's Helper", iconURL:"https://cdn.discordapp.com/avatars/955886518638088304/04d9cc2d397db8d50fcc756113ab25d2.webp?size=80"})
         .setTitle('The server IP')
-        .setDescription('Did you forget the IP, well its here')
+        .setDescription('Did you forget the IP?')
         .addFields(
-            {name:"Java", value:"pepionline.dimitrodam.com "},
+            {name:"Java", value:"pepionline.dimitrodam.com"},
             {name:"Bedrock", value:"54.39.250.160"},
             {name:"Port", value:"25581"},
             {name:"Are you on console?", value:"Refer to `p!console`"}
