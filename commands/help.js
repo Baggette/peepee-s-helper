2fd329783a35429464e5d92933bccd4c7e132487
gabe1cragger <84940492+gabe1cragger@users.noreply.github.com>
2022-07-14 00:57:17 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,10 +8,10 @@ module.exports ={
         .setAuthor({name:"Peepee's Helper", iconURL:"https://cdn.discordapp.com/avatars/955886518638088304/04d9cc2d397db8d50fcc756113ab25d2.webp?size=80"})
         .setTitle("Command list for peepee's helper")
         .addFields(
-            {name:"`p!about`", value:"Information about this origins of this server"},
+            {name:"`p!about`", value:"Information about the origins of this server"},
             {name:"`p!appeals`", value:'Some basic information on how to appeal a ban'},
-            {name:"`p!commands`", value:"Get a list of useful ingame commands for The PepiOnLine SMP"},
-            {name:"`p!economy`", value:"You can see some prices for the ingame sellable items"},
+            {name:"`p!commands`", value:"Get a list of useful in-game commands"},
+            {name:"`p!economy`", value:"View basic prices for the in-game sellable items"},
             {name:"`p!grief`", value:"Some basic information on how to report a grief"},
             {name:"`p!help`", value:"This command"},
             {name:"`p!ip`", value:"Displays the server IP"},
