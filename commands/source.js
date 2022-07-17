5d30886754fefd346a64f08638f0d837512ac1b6
gabe1cragger <84940492+gabe1cragger@users.noreply.github.com>
2022-07-16 20:16:01 -0400

    Update source.js

--- commands/source.js
+++ commands/source.js
@@ -9,7 +9,7 @@ module.exports ={
         .setTitle("Peepee's Helper Source code")
         .setURL('https://github.com/Baggette/peepee-s-helper')
         .setAuthor({name: "Peepee's Helper", iconURL:'https://cdn.discordapp.com/avatars/316986390120235018/a_7427cfdfed85a62c25ca36396b215ab0.gif?size=80', url:'https://github.com/Baggette/peepee-s-helper'})
-        .setDescription("This is the source code for this bot, if you's like to contribute feel free, Pull Requests and issues are accepted. \n You don't even need to know how to code! \n Even suggesting features of fixing spelling errors are nice \n https://github.com/Baggette/peepee-s-helper")
+        .setDescription("This is the source code for the bot. If you'd like to contribute, feel free! Pull requests and issues are accepted. \n You don't even need to know how to code! \n Even suggesting features or fixing spelling errors is very helpful \n https://github.com/Baggette/peepee-s-helper")
         .setTimestamp()
         message.channel.send({embeds:[embed]})
     }
