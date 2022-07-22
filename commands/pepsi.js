3d3605887bafaf77f5625a3eb4dc9cb0d694351d
baggette <play.email200620062006@gmail.com>
2022-07-21 19:01:24 -0500

    forgot to do this

--- commands/pepsi.js
+++ commands/pepsi.js
@@ -9,12 +9,13 @@ module.exports={
         .setDescription("some info about the built in economy for this bot")
         .addFields(
             {name:"`p!work`", value:"Work for more pepsi coin"},
-            {name:"`p!daily", value:"Claim a set amount of pepsi coin each day"},
+            {name:"`p!daily`", value:"Claim a set amount of pepsi coin each day"},
             {name:"`p!weekly`", value:"Claim a fairly decent amout of pepsi coin weekly"},
             {name:"`p!balance`", value:"See your current balance"},
             {name:"`p!deposit`", value:"Deposit pepsi coin into your bank account"},
             {name:"`p!withdraw`", value:"Withdraw money from your bank"},
-            {name:"`p!shop`", value:"If unsure what to do run `p!shop help`"},     
+            {name:"`p!shop`", value:"If unsure what to do run `p!shop help`"},
+            {name:"`p!lb`", value:"See a leaderboard for pepsi coin"}     
         )
         .setTimestamp()
         message.channel.send({embeds:[embed]})
