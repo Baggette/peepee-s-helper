5433815a5edb4b0e8e6aca5683cd09c2df2853d6
baggette <play.email200620062006@gmail.com>
2022-07-23 23:17:34 -0500

    disclaimer

--- commands/queue.js
+++ commands/queue.js
@@ -22,7 +22,7 @@ module.exports={
         .setColor("#FF0000")
         .setDescription(
           `${(
-            `Here's the server queue${
+            `Here's the server queue (will only show 20 at a time)${
               queue.songs.length > 20
                 ? `1-20/${queue.songs.length}`
                 : queue.songs.length
