60a5b49d0c17fb8cd55ca28328b32f43ade26b24
baggette <play.email200620062006@gmail.com>
2022-07-24 15:38:12 -0500

    blacklist some cmds in mc chat

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ client.on('ready', () => {
 client.on("messageCreate", (message) => {
     const args = message.content.slice(Prefix.length).split(/ +/);
       const command = args.shift().toLowerCase();
-    if (message.content.startsWith(Prefix) && message.channel.id === "872185514885791796" && !commandWhitelist.includes(command)){
+    if (message.content.startsWith(Prefix) && message.channel.id === "872185514885791796" || message.channelId === "875353517387292682" && !commandWhitelist.includes(command)){
           message.reply("Please do not use bot commands in general, use <#873623280177799198> instead.") 
           .then(msg => {
             setTimeout(() => msg.delete(), 5000)
@@ -68,7 +68,7 @@ client.on('messageCreate', (message) => {
             setTimeout(() => msg.delete(), 5000)
           })
       };
-      if(message.channelId === "872185514885791796" && !commandWhitelist.includes(command))return
+      if(message.channelId === "872185514885791796" || message.channelId === "875353517387292682" && !commandWhitelist.includes(command))return
     
       if (!client.commands.get(command)) {
           return
