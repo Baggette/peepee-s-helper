0dbb07db5cd16c9bc0774222f604eeba3b509bc1
baggette <play.email200620062006@gmail.com>
2022-07-26 17:15:10 -0500

    fixes

--- commands/weekly.js
+++ commands/weekly.js
@@ -1,6 +1,5 @@
 const Economy = require('discord-economy-super');
 const {EmbedBuilder, embedLength} = require('discord.js');
-const { execute } = require('./pepsi');
 const eco = new Economy({
     storagePath: './storage.json', // Full path to a JSON File. Default: './storage.json'.
     checkStorage: true, // Checks the if database file exists and if it has errors. Default: true.
