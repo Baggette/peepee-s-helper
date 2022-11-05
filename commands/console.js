adb016993ea9a9efe6c089c1823e154319232286
baggette <58569691+Baggette@users.noreply.github.com>
2022-11-05 21:45:31 +0000

    fix

--- commands/console.js
+++ commands/console.js
@@ -4,9 +4,9 @@ module.exports ={
     description:"Displays instuctions for how to join on console",
     execute(client, message, args){
         const embed = new EmbedBuilder()
-        .setColor("#FF0000")
         .setAuthor({name:"Peepee's Helper", iconURL:"https://cdn.discordapp.com/avatars/955886518638088304/04d9cc2d397db8d50fcc756113ab25d2.webp?size=80"})
         .setTitle("How to Join on a Console")
+        .setColor("#FF0000")
         .addDescription("This page should provide all of the information necessary to join via console (Xbox, PlayStation, Nintendo Switch, etc)", "https://wiki.geysermc.org/geyser/using-geyser-with-consoles/")
         .setTimestamp()
         message.channel.send({embeds:[embed]})
