a4c7f84461bc68cf423b5906ec5bd6d23c99425c
Baggette <play.email200620062006@gmail.com>
2022-11-11 13:35:29 -0600

    I fogor

--- slash/pepi.js
+++ slash/pepi.js
@@ -8,10 +8,18 @@ module.exports={
         await interaction.deferReply()
         const guild = await client.guilds.fetch(interaction.guildId)
         const pepi = await guild.presences.resolve("316986390120235018")
+        const custom_status = pepi.activities[0]?.state ?? "`No status`"
+        const activity_name = pepi.activities[1]?.name ?? "`No activity name`"
+        const activity_details = pepi.activities[1]?.details ?? "`No activity details`"
         if(pepi.status == "online"){
             const embed = new EmbedBuilder()
             .setTitle("PepiOnLine")
             .setDescription("Pepi is online")
+            .addFields(
+                {name:"Custom status", value:`${custom_status}`, inline: true},
+                {name:"Activity", value:`${activity_name}`, inline: true},
+                {name:"Activity details", value:`${activity_details}`, inline: true}
+            )
             .setColor("#287e29")
             .setTimestamp()
             await interaction.editReply({embeds:[embed]})
@@ -26,6 +34,11 @@ module.exports={
             const embed = new EmbedBuilder()
             .setTitle("PepiDnD")
             .setDescription("Pepi is DnD")
+            .addFields(
+                {name:"Custom status", value:`${custom_status}`, inline: true},
+                {name:"Activity", value:`${activity_name}`, inline: true},
+                {name:"Activity details", value:`${activity_details}`, inline: true}
+            )
             .setColor("#f04747")
             .setTimestamp()
             await interaction.editReply({embeds:[embed]})
@@ -33,6 +46,11 @@ module.exports={
             const embed = new EmbedBuilder()
             .setTitle("PepiIdle")
             .setDescription("Pepi is idle")
+            .addFields(
+                {name:"Custom status", value:`${custom_status}`, inline: true},
+                {name:"Activity", value:`${activity_name}`, inline: true},
+                {name:"Activity details", value:`${activity_details}`, inline: true}
+            )
             .setColor("#faa61a")
             .setTimestamp()
             await interaction.editReply({embeds:[embed]})
