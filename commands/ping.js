849011bd468d3bd9ef70983d8d8f4358cf7a2f78
Baggette <play.email200620062006@gmail.com>
2022-11-11 13:32:55 -0600

    both slash ping command and text ping are the same now

--- commands/ping.js
+++ commands/ping.js
@@ -1,18 +1,22 @@
-const {EmbedBuilder} = require('discord.js')
+const {EmbedBuilder } = require('discord.js');
+const { send } = require('node:process');
 const prettyMilliseconds = require('pretty-ms')
-module.exports ={
-    name:"ping",
-    description:"The PeePee's helper ping",
-    execute(client, message, args){
-        const embed = new EmbedBuilder()
-        .setColor("#FF0000")
-        .setAuthor({name:"Peepee's Helper", iconURL:"https://cdn.discordapp.com/avatars/955886518638088304/04d9cc2d397db8d50fcc756113ab25d2.webp?size=80"})
-        .setTitle("The Peepee's Helper bot latency and uptime ")
+const wait = require('node:timers/promises').setTimeout;
+module.exports = {
+	name:'ping',
+	description:'Shows bot latency',
+	async execute(client, message, args) {
+		const sent = await message.channel.send({ content: 'Pinging...', fetchReply: true });
+		wait(4000)
+		const embed = new EmbedBuilder()
+        .setColor("#ff0000")
+        .setTitle("Peepee's helper latency and uptime ")
         .addFields(
             {name:"Bot latency", value:`${client.ws.ping}` + 'ms'},
-            {name:"Bot uptime", value:`${prettyMilliseconds(client.uptime)}`}
+            {name:"Bot uptime", value:`${prettyMilliseconds(client.uptime)}`},
+			{name:"Round trip latency", value: `${sent.createdTimestamp - message.createdTimestamp}ms`}
             )
         .setTimestamp()
-        message.channel.send({embeds:[embed]})
-    }
-}
+		await sent.edit({embeds:[embed]});
+	},
+};
